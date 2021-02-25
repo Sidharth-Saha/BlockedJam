869e01cb7044c70a429b6eced6bbb9bfb775234c
indinin <55898529+indinin@users.noreply.github.com>
2021-02-25 16:34:25 -0700

    Build 1
    First build of ui elements

---
+++ Assets/Collin/Scripts/toggleMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class toggleMenu : MonoBehaviour
+{
+    [SerializeField] bool main_menu;
+    Canvas menu;
+    Boolean inMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = GetComponent<Canvas>();
+        inMenu = false;
+        menu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (main_menu)
+            {
+                inMenu = !inMenu;
+                Time.timeScale = (inMenu) ? 0 : 1;
+                menu.enabled = inMenu;
+            }
+            else
+            {
+                menu.enabled = false;
+            }
+        }
+            
+    }
+}
