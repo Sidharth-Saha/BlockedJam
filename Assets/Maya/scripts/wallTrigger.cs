e56173c6d867f63374e622208e99f0cfbdda1852
Maya Duarte <miduart2@asu.edu>
2021-02-28 14:28:30 -0700

    adding obstacles
    import files

---
+++ Assets/Maya/scripts/wallTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallTrigger : MonoBehaviour
+{
+    public GameObject wall;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Grab")
+        {
+            wall.GetComponent<Animator>().Play("wallDescend");
+            this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
