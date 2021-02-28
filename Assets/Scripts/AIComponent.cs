00893731747c949336e57f5960ccfca4f3723f2b
Sidharth Saha <ssaha2000@gmail.com>
2021-02-28 11:16:34 -0700

    Create AIComponent.

---
+++ Assets/Scripts/AIComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIComponent : MonoBehaviour
+{
+    // Reference to the attached nav mesh agent
+    NavMeshAgent m_NavMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get reference to attached nav mesh component
+        m_NavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
